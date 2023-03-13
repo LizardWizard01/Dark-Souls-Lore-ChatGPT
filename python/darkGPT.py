9a1590db77e261e4cacac406664a379a35cf2736
TylerAkam <tza13@psu.edu>
2023-03-13 11:12:24 -0400

    added python for dr. b to look at

--- python/darkGPT.py
+++ python/darkGPT.py
@@ -1,1 +1,6 @@
-
+import openpyxl
+wb = openpyxl.load_workbook('dsexcel.xlsx')
+type(wb)
+wb.sheetnames()
+sheet = wb['Items']
+# I'm getting "list object is not callable" as an error here, and not sure what it is talking about.
