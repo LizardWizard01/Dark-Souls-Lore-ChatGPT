ebc17679d47044fb395205d01d08a205e1a3432b
ebeshero <ebbondar@gmail.com>
2023-03-15 00:51:31 -0400

    one more addition to the Python

--- python/darkGPT.py
+++ python/darkGPT.py
@@ -1,7 +1,7 @@
 import openpyxl
 from openpyxl import load_workbook
 wb = openpyxl.load_workbook(filename='dsexcel.xlsx')
-type(wb)
+print(type(wb))
 wb.sheetnames
 print(wb.sheetnames)
 sheet = wb['Items']
