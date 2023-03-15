3c5970f2f351a3a7f83bfbc303f8234a467cabd3
TylerAkam <tza13@psu.edu>
2023-03-15 10:28:21 -0400

    adding new files for data

--- python/darkGPT.py
+++ python/darkGPT.py
@@ -1,6 +1,11 @@
 import openpyxl
 wb = openpyxl.load_workbook('dsexcel.xlsx')
 type(wb)
-wb.sheetnames()
+wb.get_sheet_names()
 sheet = wb['Items']
 # I'm getting "list object is not callable" as an error here, and not sure what it is talking about.
+tuple(sheet['A1':'M850'])
+for cellObj in sheet.columns[5]:
+    print(cellObj.value)
+# We want columns B, E, and H
+# print(tuple(sheet))
