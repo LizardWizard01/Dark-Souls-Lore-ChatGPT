3a24b5c28f1f012172b48615bd96752765871dbc
ebeshero <ebbondar@gmail.com>
2023-03-15 00:43:50 -0400

    repaired python start for scraping

--- python/darkGPT.py
+++ python/darkGPT.py
@@ -1,6 +1,8 @@
 import openpyxl
-wb = openpyxl.load_workbook('dsexcel.xlsx')
+from openpyxl import load_workbook
+wb = openpyxl.load_workbook(filename='dsexcel.xlsx')
 type(wb)
-wb.sheetnames()
+wb.sheetnames
+print(wb.sheetnames)
 sheet = wb['Items']
 # I'm getting "list object is not callable" as an error here, and not sure what it is talking about.
