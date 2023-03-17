4ed9c8e3ad70662b5ee1b0f5be00d6142a97ea32
Stephen Catledge <sxc6227@psu.edu>
2023-03-17 11:36:43 -0400

    Updating main branch

--- python/darkGPT.py
+++ python/darkGPT.py
@@ -1,1 +1,12 @@
+import openpyxl
+from openpyxl import load_workbook
+wb = openpyxl.load_workbook('dsDesc.xlsx')
+type(wb)
+sheet = wb.get_sheet_by_name('Items')
+#tuple(sheet['A1':'M850'])
+#for cellObj in sheet.columns[E]:
+#    print(cellObj.value)
+
+
+
 
