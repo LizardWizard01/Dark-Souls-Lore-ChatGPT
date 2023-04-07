81fe8828fca694656147fbc3e7ef684e6574cd4b
ebeshero <ebbondar@gmail.com>
2023-04-07 08:57:17 -0400

    output TSV network

--- python/pandas-NLP-to-NA.py
+++ python/pandas-NLP-to-NA.py
@@ -22,7 +22,16 @@ for row in df.iterrows():
     tokens = nlp(stringDesc)
     # This should run spaCy nlp over each row, the third (description) column that we imported
     for ent in tokens.ents:
-        new_row = [row[1][4], ent.text, ent.label_]
+        new_row = [row[1][4], row[1][1], 'connected to', ent.text, ent.label_]
         print(new_row)
         rows.append(new_row)
+
+# ebb: Let's output this as a TSV with network data!
+# First we send it to Pandas data frames to make a nice grid structure:
+# Create a new DataFrame with the named entities
+result = pd.DataFrame(rows, columns=['Name', 'Type', 'Connection', 'Named Entity', 'Entity Type'])
+
+# Output the result as TSV
+result.to_csv('darksouls-network.tsv', sep='\t', index=False)
+
 
