556b0abbecc1b39f9aecf49e4de6d22353adc597
ebeshero <ebbondar@gmail.com>
2023-04-17 21:44:59 -0400

    ai-training Python now outputs JSONL for fine-tuning

--- python/aiTraining.py
+++ python/aiTraining.py
@@ -13,7 +13,7 @@ config.read('config.cfg')
 # openai.api_key_path = 'config.cfg'
 API_KEY = config.get('openai', 'api_key')
 openai.api_key = API_KEY
-print(openai.Model.list())
+# print(openai.Model.list())
 
 # list models
 models = openai.Model.list()
@@ -21,19 +21,23 @@ models = openai.Model.list()
 # print the first model's id
 print(models.data[0].id)
 
+# ebb: I think we were missing a step to retrieve a model.
+print(openai.Model.retrieve("ada"))
+
 # create a completion
 completion = openai.Completion.create(model="ada", prompt="You are an assistant that generates new items that could function within the dark souls lore, but is entirely fictional and is not actually in the game")
 
 # print the completion
-# print(completion.choices[0].text)
+print(completion.choices[0].text)
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are an assistant that generates new items that could function within the dark souls lore, but is entirely fictional and is not actually in the game"},
-    # {"role": "system", "content":},
-    {"role": "user", "content": "What is your function?"}
-  ])
+#
+# completion = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are an assistant that generates new items that could function within the dark souls lore, but is entirely fictional and is not actually in the game"},
+#     # {"role": "system", "content":},
+#     {"role": "user", "content": "What is your function?"}
+#   ])
 
 # openai.File.create(
 #   file=open("ds-text.jsonl", "rb"),
@@ -41,6 +45,8 @@ completion = openai.ChatCompletion.create(
 # )
 command = ["openai", "tools", "fine_tunes.prepare_data", "-f", localTraining]
 subprocess.run(command, check=True)
+# ebb: The lines above prepare the JSONL file from JSON.
+
 # openai.fine_tunes.create -t "fine-tune.json" -m 'ada'
 # openai.FineTune.create(training_file="fine-tune.json")
-print(completion.choices[0].messages)
+# print(completion.choices[0].messages)
